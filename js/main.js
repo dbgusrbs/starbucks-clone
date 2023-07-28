e2017315e9c571bedf96b7ed8b9898fa8c724968
dbgusrbs <dbgusrbs6159@naver.com>
2023-07-28 12:25:12 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', () => {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(() => {
   if (window.scrollY > 500) {
@@ -76,8 +61,4 @@ promotionToggleBtn.addEventListener('click', () => {
   } else {
     promotionEl.classList.remove('hide');
   }
-})
-
-// 올해가 몇 년도인지 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
+});
